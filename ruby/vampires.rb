e269becd1c13a93e3d797d2a729ab5038d14fdf8
Dan Dobrick <dandobrick@gmail.com>
2016-07-05 10:49:28 -0500

    Add multiple employee capability

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,31 +1,41 @@
+puts "How many employees are going to be tested?"
+num_employees = gets.chomp.to_i
 
-puts "What is your name?"
-name = gets.chomp
+while num_employees >0
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "How old are you?"
-age = gets.chomp.to_i
+  puts "How old are you?"
+  age = gets.chomp.to_i
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+  puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-health_choice = gets.chomp
+  puts "Would you like to enroll in the company's health insurance?"
+  health_choice = gets.chomp
 
-correct_age = Time.now.year-birth_year
+  correct_age = Time.now.year-birth_year
 
-result = "Results inconclusive."
+  result = "Results inconclusive."
 
-if (age == correct_age) && (garlic == "yes" || health_choice == "yes")
-  result = "Probably not a vampire"
-elsif age != correct_age && (garlic == "no" || health_choice == "no")
-  result = "Probably a vampire"
-elsif age != correct_age && garlic == "no" && health_choice == "no"
-  result = "Almost certainly a vampire" 
+  if (age == correct_age) && (garlic == "yes" || health_choice == "yes")
+    result = "Probably not a vampire"
+  elsif age != correct_age && (garlic == "no" || health_choice == "no")
+    result = "Probably a vampire"
+  end
+  if age != correct_age && garlic == "no" && health_choice == "no"
+    result = "Almost certainly a vampire" 
+  end
+  if ['Drake Cula', 'Tu Fang'].include? name
+    result = "Definitely a vampire"
+  end
+  puts result
+  num_employees -=1
+  if num_employees >=1
+    puts "\nHave the next employee sit at the computer and press \'enter\'"
+    gets
+  end
 end
-if ['Drake Cula', 'Tu Fang'].include? name
-  result = "Definitely a vampire"
-end
-puts result
