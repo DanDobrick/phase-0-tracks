9b2a97fec8e0f8f111f7362ceb151fbec1a920aa
Dan Dobrick <dandobrick@gmail.com>
2016-07-26 11:23:36 -0500

    Create flight module and convert shout to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,39 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words.upcase + '!!!!! YAY!!!!!'
+#   end
+# end
+
+# puts Shout.yell_happily('I love coding')
+
+# puts Shout.yell_angrily('I don\'t want to go home')
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words + "!!!" + ":("
   end
 
-  def self.yell_happily(words)
-    words.upcase + '!!!!! YAY!!!!!'
+  def yell_happily(words)
+    words.upcase + '!!!!!!!!! YAY!!!!!!!'
   end
 end
 
-puts Shout.yell_happily('I love coding')
+class Tears_for_fears
+  include Shout
+end
 
-puts Shout.yell_angrily('I don\'t want to go home')
+class The_isley_brothers
+  include Shout
+end
+
+roland_orzabal = Tears_for_fears.new
+
+puts roland_orzabal.yell_happily('Shout, Shout, Let it all out. These are the things I can do without')
+
+ronald_isley = The_isley_brothers.new
+
+puts ronald_isley.yell_happily('You know you make me wanna shout')
