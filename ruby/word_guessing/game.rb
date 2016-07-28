78021ad75cc84823a8346a237d9c8cd67eb223ad
Dan Dobrick <dandobrick@gmail.com>
2016-07-28 13:50:39 -0500

    Forgot to add the game file

--- ruby/word_guessing/game.rb
+++ ruby/word_guessing/game.rb
@@ -1,13 +1,3 @@
-
-# One user can enter a word (or phrase, if you would like your game to support that), and 
-# another user attempts to guess the word.
-# Guesses are limited, and the number of guesses available is related to the length of the word.
-# Repeated guesses do not count against the user.
-# The guessing player receives continual feedback on the current state of the word. So if the secret word is "unicorn", 
-# the user will start out seeing something like "_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" after the user enters 
-# a guess of "c".
-# The user should get a congratulatory message if they win, and a taunting message if they lose.
-
 =begin
 
 Method: Initialize
@@ -29,7 +19,7 @@ steps:  Check if the letter has been guessed before, if so return false
 output: nil
 
 Method: Update game state
-input: none
+input: letter 
 steps:  remove all instances of the letter that was guessed from the string and replace with _
         update the game state string
 output: nil
@@ -48,12 +38,87 @@ output: nil
 =end
 
 class Game
-  attr_reader :num_of_guesses, :word_to_guess, :game_board
+  attr_reader :num_of_guesses # Used for rspec testing only :word_to_guess, :game_board
 
   def initialize(word)
     @word_to_guess = word
     @num_of_guesses = word.length
-    game_board_string = "_"*word.length
-    @game_board = game_board_string.split('')
+    @game_board_string = "_"*word.length
+    @game_board = @game_board_string.split('')
+  end
+
+  def update_game_state(index_of_letter)
+    @game_board[index_of_letter] = @word_to_guess[index_of_letter]
+    @word_to_guess[index_of_letter] = "_"
+  end
+
+  #The following two methods (update_guess_count and check_guess) were lumped together at one point, 
+  #but I separated them so my methods would have one job
+  def update_guess_count(letter_guessed)
+    unless @game_board.include?(letter_guessed)
+      @num_of_guesses -=1
+    end
+  end
+
+  def check_guess(letter_guessed)
+    while @word_to_guess.include?(letter_guessed)
+        letter_index = @word_to_guess.index(letter_guessed)
+        update_game_state(letter_index)
+      end
+  end
+
+  def display
+    @game_board_string = ''
+    @game_board.each {|character| @game_board_string += character+" "}
+    puts @game_board_string
+  end
+
+  def is_over?
+    if @num_of_guesses == 0
+      true
+    elsif @word_to_guess == ('_'*@word_to_guess.length)
+      true
+    else
+      false
+    end
+  end
+
+  def did_win?
+    if @word_to_guess == ('_'*@word_to_guess.length)
+      true
+    else
+      false
+    end
   end
 end
+
+puts "Hello and welcome to a weird version of hangman!"
+puts "Player 1, please choose a word for player two to guess. Player 2, please turn your head."
+
+word = gets.chomp
+game = Game.new(word)
+
+while !game.is_over?
+  puts "Player 2, you have #{game.num_of_guesses} guess left"
+  game.display
+
+  puts "Player 2, make a guess"
+
+  player_2 = gets.chomp
+
+  game.update_guess_count(player_2)
+  game.check_guess(player_2)
+end
+
+if game.did_win?
+  puts
+  puts "Good job Player 2, you won!"
+else
+  puts
+  puts "Try harder next time Player 2!"
+end
+
+
+
+
+
