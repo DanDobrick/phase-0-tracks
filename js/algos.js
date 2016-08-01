4a753ae2a1cba31e052ffe8f754ec94caa5af3ac
Dan Dobrick <dandobrick@gmail.com>
2016-08-01 18:27:17 -0500

    Pseudo Code first version of key-value matcher, try to be more efficient tomorrow

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,3 @@
-// ["long phrase","longest phrase","longer phrase"], 
-// it would return "longest phrase".
-
 /* Pseudo code
 
 input: Array of strings
@@ -26,6 +23,43 @@ function find_longest(array){
   return longest;
 }
 
+/*
+In algos.js, write a function that takes two objects and checks to see if the objects share at 
+least one key-value pair. (If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54},
+the function  would return true, because at least one key-value pair matches between the two objects. If no pairs match
+(and keep in mind that the two objects may not even have any of the same keys), the function should return false. To make 
+your life 
+easier, don't worry about whether a property is a string ('age') or an identifier name (age). Those can be considered 
+equivalent. Again, try to reason through the problem using the basics you've already learned, rather than looking up slick
+search functions that will do the job for you. We'd rather see you write code that you actually understand!
+
+Pseudo Code
+
+input: two key, value pairs
+steps: get all the keys from each hash
+      for each key, check if they match any of the keys from the other object
+      if they do
+        check if their values match
+        if they do
+          return true
+        if they don't
+          move on
+      if they don't move on
+
+      If, after all that, the function has not returned true, return false
+return: boolean
+
+*/
+
+//Function tests
 long = find_longest(["long phrase","longest phrase","longer phrase"]);
+
+console.log(long)
+
+long = find_longest(["1234567890","123","1234565"]);
+
+console.log(long)
+
+long = find_longest(["12345","12345","8675309", "8675308"]);
 
 console.log(long)
