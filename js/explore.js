784e313df7d4e0323f23f4acace64355aa34638a
Dan Dobrick <dandobrick@gmail.com>
2016-08-01 12:05:40 -0500

    Remove extra comments

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,4 @@
 /*
-This isn't a task specific to JavaScript -- your pseudocode should be in plain English and use phrases like "for each" instead of JavaScript-specific terms. Remember that you can add strings in JavaScript, so '' + 'a' would result in the string 'a'. Take your time. This is the most valuable skill in programming: being able to think through a problem logically.
-Under your commented pseudocode, implement your function in JavaScript. Test it with driver code if you like. Anytime you'd like to run your code, you can run node explore.js from the js folder in your terminal, similar to how you run Ruby programs.
-Add driver code that calls the function in order to reverse a string of your choice (as long as it's not a palindrome!), and stores the result of the function in a variable.
-Add driver code that prints the variable if some condition is true. The condition can be a silly one, like 1 == 1.
-
 as many times as there are characters in the string, take the last letter and put it into a new string.
 Then move to the second to last letter etc.
 return the new string
