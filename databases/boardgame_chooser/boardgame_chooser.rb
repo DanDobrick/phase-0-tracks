26f626433b02ee8402527b6b19e5660fca159e51
Dan Dobrick <dandobrick@gmail.com>
2016-08-08 17:21:35 -0500

    Change column titles in owner table to be less ambigous; fit SQL string to select a random game

--- databases/boardgame_chooser/boardgame_chooser.rb
+++ databases/boardgame_chooser/boardgame_chooser.rb
@@ -5,22 +5,26 @@ require 'sqlite3'
 db = SQLite3::Database.new("boardgames.db")
 db.results_as_hash = true
 
+#Prints game information
+def print_game(hash_to_print)
+  hash_to_print.each do |game|
+    game.each do |key, val|
+      if !key.is_a? Integer
+        puts "#{key} is #{val}"
+      end
+    end
+    puts
+  end
+end
+
 # Choose a random game and print out who owns that game
 num_of_games = db.execute("SELECT COUNT(id) FROM games")
-#converting sql result to integer
-num_of_games = num_of_games[0]["COUNT(id)"]
-num_of_games
+#converting SQL result from a hash inside an array to integer
+num_of_games = num_of_games[0][0]
 
-#Print out of all values of each game
-# get_all_games = db.execute(<<-SQL
-#   SELECT * FROM games;
-#   SQL
-#   )
-# get_all_games.each do |game|
-#   game.each do |key, val|
-#     if !key.is_a? Integer
-#       puts "#{key} is #{val}"
-#     end
-#   end
-#   puts
-# end
+random_number = rand(num_of_games) + 1
+
+# Selects game based on id and prints certain information
+random_game = db.execute( "SELECT games.name, games.player_min, games.player_max, owners.owner_name FROM games JOIN owners ON games.owner_id = owners.id WHERE games.id = ?", [random_number])
+
+print_game(random_game)
