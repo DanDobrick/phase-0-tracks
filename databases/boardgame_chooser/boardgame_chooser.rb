f351db9dab06003788f0071d3a780ef87972fd44
Dan Dobrick <dandobrick@gmail.com>
2016-08-09 12:41:44 -0500

    Add random game function, seperate printing functions into short and verbose

--- databases/boardgame_chooser/boardgame_chooser.rb
+++ databases/boardgame_chooser/boardgame_chooser.rb
@@ -1,8 +1,3 @@
-# todo: change print method to print_game_short that shows name and id
-# make verbose print method which shows all the information (maybe add more to the DB?)
-# Choose a random game
-# Choose a random game with player count
-
 #require gems
 require 'sqlite3'
 
@@ -10,39 +5,49 @@ require 'sqlite3'
 db = SQLite3::Database.new("boardgames.db")
 db.results_as_hash = true
 
-def print_game(hash_to_print)
-
-  hash_to_print.each do |game|
+def remove_extra_info(orig_array)
+  #removes extra information from each game hash passed in as an array of hashes, returns a cleaned array
+  games_array = []
+  orig_array.each do |game|
+    orig_array ={}
     game.each do |key, val|
-      # This if statement gets rid of extraneous information given to us by SQL
       if !key.is_a? Integer
-        if key == 'id'
-          category = 'id: '
-          value = val.to_s
-        elsif key == 'name'
-          category = 'Name: '
-          value = val
-        elsif key == 'player_min'
-          category = 'Minimum players: '
-          value = val.to_s
-        elsif key == 'player_max'
-          category = 'Max players: '
-          value = val.to_s
-        else
-          category = ''
-          value = ''
-        end
-        puts category + value
+        orig_array[key] = val
       end
     end
+    games_array.push(orig_array)
   end
+  games_array
+end
+
+def print_game_short(array_to_print)
+  #prints game name and id when passed an array of hashes
+  array_to_print.each do |game|
+    game_name = game['name']
+    game_id = game['id']
+    puts "ID: " + game_id.to_s
+    puts "Name: " + game_name
+    puts 
+  end
+
+end
+
+def get_more_info(db, game_id)
+  #returns verbose information as an array from the database when passed a db and game id
+  game_info = db.execute("SELECT games.name, games.id, games.player_min, games.player_max, owners.owner_name
+    FROM games
+    JOIN owners ON games.owner_id = owners.id
+    WHERE games.id = ?", [game_id])
+  verbose_game_info = remove_extra_info(game_info)
 end
 
 def match_player_count(db, player_number)
+  #returns an array of hashes (already cleaned) that match the player number specified
   games = db.execute("SELECT games.name, games.id, games.player_min, games.player_max, owners.owner_name
     FROM games
     JOIN owners ON games.owner_id = owners.id
     WHERE games.player_min <= ? AND games.player_max >= ?", [player_number, player_number])
+  games = remove_extra_info(games)
 end
 
 def owner_name_to_id(owner_name)
@@ -54,48 +59,35 @@ def owner_name_to_id(owner_name)
     'Dan | Shaw' => 6,
     'Dave | Shaw' => 7
   }
-
   owner_id = game_owners[owner_name]
 end
 
 def enter_new_game(db, game_name, player_min, player_max, owner_name)
-    db.execute("INSERT INTO games (name, player_min, player_max, owner_id)
-      VALUES (?, ?, ?, ?)", [game_name, player_min, player_max, owner_id]
-      )
+  owner_id = owner_name_to_id(owner_name)
+  db.execute("INSERT INTO games (name, player_min, player_max, owner_id)
+    VALUES (?, ?, ?, ?)", [game_name, player_min, player_max, owner_id]
+    )
 end
 
-# num_of_games = db.execute("SELECT COUNT(id) FROM games")
-# #converting SQL result from a hash inside an array to integer
-# num_of_games = num_of_games[0][0]
-# random_number = rand(num_of_games) + 1
+def choose_random_game(db)
+  #getting number of games in the table 'games'
+  num_of_games = db.execute("SELECT COUNT(id) FROM games")
+  #converting SQL result from a hash inside an array to integer
+  num_of_games = num_of_games[0][0]
+  random_number = rand(num_of_games) + 1
 
-# # Select game based on id and prints certain information
-# random_game = db.execute( "SELECT games.name, games.player_min, games.player_max, owners.owner_name FROM games JOIN owners ON games.owner_id = owners.id WHERE games.id = ?", [random_number])
-# print_game(random_game)
+  # Select game based on id and prints certain information
+  random_game = db.execute( "SELECT games.name, games.player_min, games.player_max, owners.owner_name
+                              FROM games
+                              JOIN owners ON games.owner_id = owners.id
+                              WHERE games.id = ?", [random_number]
+                          )
+  random_game = remove_extra_info(random_game)
+end
 
-# print_game(match_player_count(5, db))
-
-# puts "WELCOME TO GAME TRACKER 5000!"
-# puts "Select a number from the options below"
-# puts '      1. Add a game to the collection
-#       2. Display games that support 
-#       3. Choose a random game based on the number of players
-#       4. Something special'
-# selection = gets.chomp
-# until selection.to_i>0 && selection.to_i<5
-#   puts "I'm sorry \"#{selection}\" is not a valid choice, please select from the following options:
-#       1. Add a game to the collection
-#       2. Choose a random game
-#       3. Choose a random game based on the number of players
-#       4. Something special"
-#   selection = gets.chomp
-# end
-# puts "How many people are playing today?"
-# players = gets.chomp
-# players = players.to_i
-# matched_games = match_player_count(db, 8)
-# num_of_games = matched_games.length
-# puts "You have #{num_of_games} games to choose from. Press any key to continue."
-# gets
-# print_game(matched_games)
+def random_game_playercount(db, player_count)
+  possible_games = match_player_count(db, player_count)
+  random_number = rand(possible_games.length) + 1
+  random_game = possible_games[random_number]
+end
 
