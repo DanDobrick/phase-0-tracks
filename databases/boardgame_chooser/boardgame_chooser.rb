1a02654a65dcb924d1ca06813ab9e74a20a65f2a
Dan Dobrick <dandobrick@gmail.com>
2016-08-09 12:54:33 -0500

    Add continuous loop until user decides to quit, choices currently print selection

--- databases/boardgame_chooser/boardgame_chooser.rb
+++ databases/boardgame_chooser/boardgame_chooser.rb
@@ -51,6 +51,7 @@ def match_player_count(db, player_number)
 end
 
 def owner_name_to_id(owner_name)
+  #Takes owner_name as a string and returns owner id for database use
   game_owners = { 'Dan' => 1,
     'Dave' => 2,
     'Shaw' => 3,
@@ -63,6 +64,7 @@ def owner_name_to_id(owner_name)
 end
 
 def enter_new_game(db, game_name, player_min, player_max, owner_name)
+  #enters a new game into the database
   owner_id = owner_name_to_id(owner_name)
   db.execute("INSERT INTO games (name, player_min, player_max, owner_id)
     VALUES (?, ?, ?, ?)", [game_name, player_min, player_max, owner_id]
@@ -78,33 +80,65 @@ def choose_random_game(db)
 
   # Select game based on id and prints certain information
   random_game = db.execute( "SELECT games.name, games.player_min, games.player_max, owners.owner_name
-                              FROM games
-                              JOIN owners ON games.owner_id = owners.id
-                              WHERE games.id = ?", [random_number]
-                          )
+    FROM games
+    JOIN owners ON games.owner_id = owners.id
+    WHERE games.id = ?", [random_number]
+    )
   random_game = remove_extra_info(random_game)
 end
 
 def random_game_playercount(db, player_count)
+  #returns a random game (extra info already removed) based on player_count
   possible_games = match_player_count(db, player_count)
   random_number = rand(possible_games.length) + 1
   random_game = possible_games[random_number]
 end
 
+#USER INTERFACE
+continue = true
 puts "Welcome to Game Organizer 5,000!"
-puts "Select a number from the options below"
-puts '      1. Add a game to the collection
-      2. Choose a random game
-      3. Choose a random game based on the number of players
-      4. Display games that support a particular player count
-      5. Find information on a certain game(NOT YET IMPLEMENTED).'
-selection = gets.chomp
-until selection.to_i>0 && selection.to_i<6
-  puts "I'm sorry \"#{selection}\" is not a valid choice, please select from the following options:
-      1. Add a game to the collection
-      2. Choose a random game
-      3. Choose a random game based on the number of players
-      4. Display games that support a particular player count
-      5. Find information on a certain game(NOT YET IMPLEMENTED)."
+while continue
+  puts "Select a number from the options below"
+  puts '      1. Add a game to the collection.
+      2. Choose a random game.
+      3. Choose a random game based on the number of players.
+      4. Display games that support a particular player count.
+      5. Find information on a certain game(NOT YET IMPLEMENTED).
+      6. Quit program'
   selection = gets.chomp
+  until selection.to_i>0 && selection.to_i<7
+    puts "I'm sorry \"#{selection}\" is not a valid choice, please select from the following options:
+      1. Add a game to the collection.
+      2. Choose a random game.
+      3. Choose a random game based on the number of players.
+      4. Display games that support a particular player count.
+      5. Find information on a certain game(NOT YET IMPLEMENTED).
+      6. Quit program"
+    selection = gets.chomp
+  end
+
+  case selection
+  when "1"
+    puts "1. Add a game to the collection."
+  when "2"
+    puts "2. Choose a random game."
+  when "3"
+    puts "3. Choose a random game based on the number of players."
+  when "4"
+    puts "4. Display games that support a particular player count."
+  when "5"
+    puts "5. Find information on a certain game(NOT YET IMPLEMENTED)."
+  when "6"
+    puts "Thank you for using Game Organizer 5000."
+    continue = false
+  else
+    puts "You should never be able to get to this point."
+  end
 end
+
+
+
+
+
+
+
